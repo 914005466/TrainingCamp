e3b0b6dd818ebd5c51aed062b309d632d09bf1f4
914005466 <91400546@qq.com>
2024-04-20 11:30:16 +0800

    first commit

---
+++ day11/js/js.js
@@ -0,0 +1,28 @@
+// let obj = {name:"王刚", address:"郑州市金水区", phone:"166372266619"}
+// console.log(obj);
+// let brr = [
+//     {name:"王刚", address:"郑州市金水区", phone:"166372266619"},
+//     {name:"李四", address:"郑州市中原区", phone:"15517276619"}
+// ];
+// console.log(brr[0]);
+// console.log(brr[1].address);
+// console.log(brr[0].phone);
+// let a = 2;
+// let b = 3;
+// console .log(a<b || a++);
+// console.log(b=a++ || a>b);
+// console.log(a);  
+let age=-10;
+if (age>=70) {
+    console.log("该退休了");
+}else if(age<70 && age>=50){
+    console.log("退什么休，再坚持一下");
+}else if (age<50 && age>=30) {
+    console.log("正值青壮年，赶紧去工作");
+}else if (age<30 && age>=18) {
+    console.log("刚工作几年啊，就想七想八");
+}else if (age<18  &&age>=0) {
+    console.log("还没成年呢，凑什么热闹");
+}else if (age<0) {
+    console.log("没出生的也来凑热闹？天堂也有业绩考核么");
+};
